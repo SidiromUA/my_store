8c8dbe09d40d2b92d745c3e163bcdcf84314f09d
Roman Golovatyi <roman@golovatiy.com>
2019-09-15 18:36:14 +0300

    Add auto-create files

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,54 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all 
+  end
+
+  # items/1 GET
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render plain: 'Page is not found', status: 404
+    end
+  end
+
+  # items/new GET
+  def new
+    @item = Item.new
+  end
+
+  # items/1/edit GET
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  # items POST
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    if @item
+      redirect_to item_path(@item)
+    else 
+      render "new"
+    end
+  end
+
+  # items/1 PUT
+  def update
+    @item = Item.find(params[:id])
+    @item.update_attributes(item_params)
+    @item.save
+    if @item
+      redirect_to item_path(@item)
+    else 
+      render "edit"
+    end
+  end
+
+  # items/1 DELETE
+  def destroy; end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:price, :name, :real, :weight, :description)
+  end
+end
